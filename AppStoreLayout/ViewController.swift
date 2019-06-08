fcb628dbd680c3f98f19a10492d5de1e7496bad6
Juan Laube <juanlaube@users.noreply.github.com>
2019-06-08 12:36:05 -0300

    Initial Commit

---
+++ AppStoreLayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppStoreLayout
+//
+//  Created by Juan Laube on 6/8/19.
+//  Copyright © 2019 Juan Laube. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
